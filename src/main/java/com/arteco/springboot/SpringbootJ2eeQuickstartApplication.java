26fc3dbca1ec5fb8515079af0ebf6c34c5b3e2cc
Ramón Arnau <rarnau@arteco-consulting.com>
2016-09-26 14:37:00 +0200

    Adaptación para despliegue en jboss

--- src/main/java/com/arteco/springboot/SpringbootJ2eeQuickstartApplication.java
+++ src/main/java/com/arteco/springboot/SpringbootJ2eeQuickstartApplication.java
@@ -1,19 +1,39 @@
 package com.arteco.springboot;
 
+import org.apache.tomcat.jdbc.pool.XADataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class SpringbootJ2eeQuickstartApplication {
+public class SpringbootJ2eeQuickstartApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJ2eeQuickstartApplication.class, args);
 	}
 
-//	@Configuration
-//	public class JerseyConfig extends ResourceConfig {
-//		public JerseyConfig() {
-//			register(IndexRestController.class);
-//		}
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<SpringbootJ2eeQuickstartApplication> applicationClass = SpringbootJ2eeQuickstartApplication.class;
+
+	@Bean
+	@Profile("devel")
+	public DataSource dataSource(Environment environment){
+		XADataSource dataSource = new XADataSource();
+		dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+		return dataSource;
+	}
 }
--- src/test/java/com/arteco/springboot/SpringbootJ2eeQuickstartApplicationTests.java
+++ src/test/java/com/arteco/springboot/SpringbootJ2eeQuickstartApplicationTests.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootJ2eeQuickstartApplicationTests {
+@ActiveProfiles("devel")
+public class SpringbootJ2eeQuickstartApplicationTests{
 
 	@Autowired
 	private PersonDao personDao;
